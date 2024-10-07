91bfe1a09960ec015e6621e2aa9f61a197234e2e
ClarenceKe <837996560@qq.com>
2024-10-07 19:13:05 +0800

    Update main_Benchmark_TRCA_PRCA.m

--- main_Benchmark_TRCA_PRCA.m
+++ main_Benchmark_TRCA_PRCA.m
@@ -10,14 +10,16 @@
 
 % The TRCA Algorithm was adapted from https://github.com/edwin465/Comparison-between-two-implementation-approaches-of-TRCA-in-Matlab
 
-% if you use this code for a publication, please cite the following paper
+% if you use this code for a publication, please cite the following paper:
 % Ke, Yufeng; Liu, Shuang; Ming, Dong (2023). Enhancing SSVEP Identification with Less Individual Calibration Data 
 % Using Periodically Repeated Component Analysis.  IEEE Transactions on Biomedical Engineering. 71(4): 1319 - 1331. https://doi.org/10.1109/TBME.2023.3333435
 %%
 
 clear
+
 % Please download the SSVEP benchmark dataset for this code
 % Wang, Y., et al. (2016). A benchmark dataset for SSVEP-based brain-computer interfaces. IEEE Transactions on Neural Systems and Rehabilitation Engineering, 25(10), 1746-1752.
+
 filepath='Benchmark Dataset\';
 files=findfilenames(filepath, 'S', 'mat');
 load('Benchmark Dataset\Freq_Phase.mat')
